53d60f3311562bd7a1a089bba065c1fff7e8c444
Michał Łazowik <mlazowik@gmail.com>
2016-05-27 17:38:17 +0200

    Separate converting

--- options/number_parser.cpp
+++ options/number_parser.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <string-utils/utils.h>
 
 #include "number_parser.h"
 
@@ -13,19 +14,8 @@ void NumberParser::parse() {
     this->stringParser.parse();
 
     std::string numberString = this->stringParser.getValue();
-    std::string::size_type first_after_number;
 
-    try {
-        this->value = std::stoi(numberString, &first_after_number);
-    } catch (std::invalid_argument &ex) {
-        throw std::invalid_argument(numberString + " is not a number");
-    } catch (std::out_of_range &ex) {
-        throw std::out_of_range(numberString + " is out of range");
-    }
-
-    if (first_after_number != numberString.length()) {
-        throw std::invalid_argument(numberString + " is not a number");
-    }
+    this->value = String::toInt(numberString);
 }
 
 int NumberParser::getValue() const {
